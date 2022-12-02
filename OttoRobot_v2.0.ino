865bff1e11553d0d8668bba0da713f8dab412f63
Aadhav Saravanan <119620882+Aadhav-Saravanan@users.noreply.github.com>
2022-12-02 14:05:33 +0100

    OttoRobot_v1.1.ino
    This version has no update it has new changed variables.

--- OttoRobot_v2.0.ino
+++ OttoRobot_v2.0.ino
@@ -1,1 +1,31 @@
+#include <Servo.h>  //Servo library
 
+Servo BL, BR, TR, TL;     //BR=Bottom Left, BR=Bottom Right, Tr=Top Right and TL=Top Left
+int servoDelay = 1000;    // delay time for the servo
+int servoAngleMax = 180;  // Max angle for the servo motor
+int servoAngleHalf = 90;  //Half angle for the sevo motor
+int servoMin = 0;         //Min angle for the servo motor
+
+void setup() {
+  BL.attach(8);
+  BR.attach(9);
+  TR.attach(10);
+  TL.attach(11);
+}
+
+void loop() {
+  BL.write(servoAngleMax);
+  BR.write(servoAngleMax);
+  TR.write(servoAngleMax);
+  TL.write(servoAngleMax);
+
+  delay(servoDelay);
+
+  BL.write(servoMin);
+  BR.write(servoMin);
+  TR.write(servoMin);
+  TL.write(servoMin);
+
+  delay(servoDelay);
+}
+//version 1.1
