dc0fed16658ea84c2ae875b56efd636c584fc7c3
Aadhav Saravanan <119620882+Aadhav-Saravanan@users.noreply.github.com>
2022-12-17 12:23:52 +0100

    OttoRobot_v2

--- OttoRobot_v2.ino
+++ OttoRobot_v2.ino
@@ -1,26 +1,32 @@
 #include <Servo.h>
 
 Servo Servo1;
-Servo Servo2;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo Servo2;
+Servo Servo3;
+Servo Servo4;
 
-int pos = 0;    // variable to store the servo position
+int pos = 0;
 
 void setup() {
   Servo1.attach(8);
-  Servo2.attach(9);  // attaches the servo on pin 9 to the servo object
+  Servo2.attach(9);
+  Servo3.attach(10);
+  Servo4.attach(11);
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    Servo1.write(pos);              // tell servo to go to position in variable 'pos'
-    Servo2.write(pos); 
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 0; pos <= 180; pos += 1) { 
+    Servo1.write(pos);
+    Servo2.write(pos);
+    Servo3.write(pos);
+    Servo4.write(pos);
+    delay(15);
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    Servo1.write(pos);              // tell servo to go to position in variable 'pos'
-    Servo2.write(pos);  
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 180; pos >= 0; pos -= 1) {
+    Servo1.write(pos);
+    Servo2.write(pos);
+    Servo3.write(pos);
+    Servo4.write(pos); 
+    delay(15);
   }
 }
